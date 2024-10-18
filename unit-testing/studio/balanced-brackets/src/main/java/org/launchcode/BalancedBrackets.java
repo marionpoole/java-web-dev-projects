a9898f6f5a8f7d2449e9c412af04758e4248efd7
Marion Poole <mapoole09@hotmail.com>
2024-10-17 19:50:15 -0500

    Completed Unit Testing

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -28,6 +28,10 @@ public class BalancedBrackets {
                 brackets--;
             }
         }
-        return brackets == 0;
+        if(brackets == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,67 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void notBalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void returnsTrueWithNonbracketCharactersWhenBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{[{]}{[}]}"));
+    }
+
+    @Test
+    public void returnsTrueWhenBalancedIfStartsWithClosingBracket() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("][]["));
+    }
+
+    @Test
+    public void returnsTrueWithNumbersWhenBracketsBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[12]123"));
+    }
+
+    @Test
+    public void returnsTrueWithLettersWhenBracketsBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[abdc]abdcd[]"));
+    }
+
+    @Test
+    public void returnsTrueWhenBracketsAreBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void returnsFalseWhenBracketsAreNotBalanced() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void validateNotNull() {
+        assertNotNull(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void returnsFalseWhenMoreClosingBracketsThanOpening() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]]]]]]["));
+    }
+
+    @Test
+    public void returnsFalseWhenMoreOpeningBracketsThanClosing() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[[[[[[[[[]"));
+    }
+
+    @Test
+    public void returnsTrueWhenNoBracketsPassedIn() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+//    @Test
+//    public void bracketsS() {
+//        assertTrue(BalancedBrackets.brackets == 0);
+//    }
 }
